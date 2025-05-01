85af41fa9e460b5ea0e0bf910dece2d23ad52664
Max Bemax <7510332@gmail.com>
2025-05-01 21:00:09 +0300

    main structure

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-textInput.addEventListener('input', event => {
-  const name = event.currentTarget.value.trim();
-  output.textContent = name === '' ? 'Anonymous' : name;
-});
